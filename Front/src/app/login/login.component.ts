c7f6a24049c0b04ec910bcb7d72a7ccbd4c18aa1
SergioGarzon <sergio.garzon@alfagl.com>
2021-10-27 22:09:34 -0500

    login

--- Front/src/app/login/login.component.ts
+++ Front/src/app/login/login.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
-enum FormField {
-  user = 'user',
-  p = 'pass'
-}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,26 +10,17 @@ enum FormField {
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup
-  formField = FormField
   pFiedType = 'password'
   pIconTooltip = 'Mostrar Contraseña'
   user: string
   pass: string
   constructor(private fb: FormBuilder,) {
-    this.form = this.buildForm()
-    this.user = this.form.value.user
+
+    this.user = 
     this.pass = '1233445'
    }
 
   ngOnInit(): void {
-  }
-
-  buildForm(): FormGroup {
-    return this.fb.group({
-      [ FormField.user ]: this.fb.control(null, [ Validators.required ]),
-      [ FormField.p ]: this.fb.control(null, [ Validators.required ]),
-    })
   }
 
   login(){
