91f110814ab2396c4a111622e7917bd8c5332fd4
CristianMojica <cristian.mojica@alfagl.com>
2021-11-04 16:45:44 -0500

    Jury completo

--- Front/src/app/jury/jury.component.ts
+++ Front/src/app/jury/jury.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms'
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-jury',
@@ -6,11 +8,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jury.component.scss']
 })
 export class JuryComponent implements OnInit {
-  constructor() { 
-      
-  }
+  selected = 'Sergio';
+  form: FormGroup;
+  constructor(
+    private FormBuilder: FormBuilder
+  ) {
+    this.buildForm();
+  
+   }
 
   ngOnInit(): void {
+  
   }
+
+  private buildForm() {
+    this.form = this.FormBuilder.group({
+      nameProject: ['', [Validators.required]],
+      codeGroup: ['', [Validators.required, Validators.minLength(2)]],
+      date: ['', [Validators.required]],
+      text: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(50)]], 
+      career: ['', [Validators.required]],
+      codeStudent: ['', [Validators.required]],
+    });
+
+    this.form.valueChanges
+    .pipe(
+      debounceTime(600)
+    )
+    .subscribe(value => {
+      console.log(value);
+    });
+  }
+  save(event: Event) {
+    event.preventDefault();
+    if(this.form.valid){
+      const value = this.form.value;
+      console.log(value);
+
+    }else{
+      this.form.markAllAsTouched();
+    }
+  }
+
+  get nameProjectField(){
+    return this.form.get('nameProject');
+  }
+  get codeGroupField(){
+    return this.form.get('codeGroup');
+  }
+  get dateField(){
+    return this.form.get('date');
+  }
+  get textField(){
+    return this.form.get('text');
+  }
+  get careerField(){
+    return this.form.get('career');
+  }
+  get codeStudentField(){
+    return this.form.get('codeStudent');
+  }
+
 
 }
